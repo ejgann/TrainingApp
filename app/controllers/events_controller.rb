c87846ebab00159a7d1d34991e13fae76547c02c
Emily Gann <aspentree79@gmail.com>
2020-07-13 08:45:17 -0400

    added view files and controller actions for events and workouts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,29 @@
 class EventsController < ApplicationController
+    def index
+        @events = Event.all
+    end
+    
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+            redirect_to events_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @event = Event.find_by(id: params[:id])
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:title, :category, :description, :length)
+    end
+
 end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,29 @@
 class WorkoutsController < ApplicationController
+
+    def index
+        @workouts = Workout.all
+    end
+
+    def new
+        @workout = Workout.new
+    end
+
+    def create
+        @workout = Workout.new(workout_params)
+        if @workout.save
+            redirect_to workouts_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @workout = Workout.find_by(id: params[:id])
+    end
+
+    private
+
+    def workout_params
+        params.require(:workout).permit(:name, :category, :duration, :intensity)
+    end
 end
