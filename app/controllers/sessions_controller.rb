d48f27062bbf4aa6b04ae6fc9d30b1de97e0a510
Emily Gann <aspentree79@gmail.com>
2020-07-19 13:39:05 -0400

    Fixed error in sessions create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
     end
     
     def create
-        byebug
-        user = User.find_by(username: params[:user][:username])
+        @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id 
             redirect_to user_path(@user)
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'sessions#index'
 
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-
   delete '/logout', to: 'sessions#destroy'
   
   resources :trainings
