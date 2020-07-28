968ff080389a2ff4bdd70e1771bcf0b17a4e2cd4
Emily Gann <aspentree79@gmail.com>
2020-07-28 16:46:00 -0400

    Made small change to trainings new view to indicate workout for which a training is being recorded

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,8 +1,8 @@
 class Training < ApplicationRecord
   belongs_to :event
   belongs_to :workout
-
-  # accepts_nested_attributes_for :workout
+  
+  
 
   # this creates a custom writer method called "workout_attributes=(attributes)", which is referenced in training_params
 
