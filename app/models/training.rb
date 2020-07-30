6ba89612e284ff11c937002370075dd5f14b589f
Emily Gann <aspentree79@gmail.com>
2020-07-30 12:25:10 -0400

    Added scope method :most_popular to Workout model

--- app/models/training.rb
+++ app/models/training.rb
@@ -4,12 +4,13 @@ class Training < ApplicationRecord
   validates :date, :notes, presence: true
   validates_associated :workout
   
+  # scope methods
+  # scope :most_recent_trainings, -> (limit) { order("created_at desc").limit(limit) }
 
   # this creates a custom writer method called "workout_attributes=(attributes)", which is referenced/used in training_params
 
   def workout_attributes=(workout_attributes)
     workout = Workout.find_or_create_by(workout_attributes)
-    
     if workout.valid?   
     self.workout = workout
     end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -8,4 +8,11 @@ class Workout < ApplicationRecord
   validates :duration, numericality: true
   validates :intensity, numericality: { greater_than: 0, less_than: 6 }
 
+# scope method
+  scope :most_popular, -> { Workout.joins(:trainings).group(:id).order("count(workout_id) desc") }
+  # want to order the 'all workouts' view by popularity (i.e. number of trainings associated with that workout)
+  # to count the number of trainings of an associated workout => @workout.trainings.count
+  # .order -- desc
+  # because it involves a join table, we have to incorporate joins + model into the scope method
+
 end
